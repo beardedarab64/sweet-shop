5b40c0ec7c93078c204ec81f1895ca2fe71ad864
jeka-js <jeka7js@gmail.com>
2013-02-25 22:54:10 +0200

    Try to add popup menu to the goods list items

--- TreeViewGoods.cpp
+++ TreeViewGoods.cpp
@@ -11,6 +11,17 @@ TreeViewGoods::TreeViewGoods()
     append_column( "Наименование", treeColumns.name );
     append_column( "Цена", treeColumns.price );
     append_column( "Ед.Изм.", treeColumns.item );
+
+    Gtk::MenuItem* item = Gtk::manage( new Gtk::MenuItem( "Подробнее" ) );
+    item->signal_activate().connect( sigc::mem_fun( *this, &TreeViewGoods::on_menu_file_popup_generic ) );
+    menuPopup.append( *item );
+    menuPopup.accelerate( *this );
+    menuPopup.show_all();
+
+#ifndef GLIBMM_DEFAULT_SIGNAL_HANDLERS_ENABLED
+  signal_button_press_event()
+    .connect(sigc::mem_fun(*this, &TreeViewGoods::on_button_press_event), false);
+#endif
 }
 
 void TreeViewGoods::remove_all_rows()
@@ -27,4 +38,25 @@ void TreeViewGoods::append_data( Glib::ustring id, Glib::ustring name, Glib::ust
     row[ treeColumns.price ] = price;
     row[ treeColumns.item ] = item;
 }
+
+void TreeViewGoods::on_menu_file_popup_generic()
+{
+    g_print( "Yeah!\n" );
+}
+
+bool TreeViewGoods::on_button_press_event( GdkEventButton *event )
+{
+    g_print("попал таки\n");
+    bool return_value = false;
+    return_value = TreeView::on_button_press_event( event );
+
+    if( ( event->type == GDK_BUTTON_PRESS ) && ( event->button == 3 ) )
+    {
+        g_print( "suck\n" );
+        menuPopup.popup( event->button, event->time );
+    }
+
+    return return_value;
+}
+
 
--- TreeViewGoods.h
+++ TreeViewGoods.h
@@ -11,9 +11,14 @@ class TreeViewGoods: public Gtk::TreeView
         void remove_all_rows();
         void append_data( Glib::ustring, Glib::ustring, Glib::ustring, Glib::ustring );
 
+    protected:
+        virtual bool on_button_press_event( GdkEventButton * );
+        void on_menu_file_popup_generic();
+
     private:
         GoodsColumns treeColumns;
         Glib::RefPtr<Gtk::ListStore> treeRecords;
+        Gtk::Menu menuPopup;
 };
 
 #endif // TREEVIEWGOODS_H
