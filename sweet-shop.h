7bbacd3afd17645a55d5bba9957187cd80ef386e
jeka-js <jeka7js@gmail.com>
2013-02-28 20:15:55 +0200

    Added header file for global defines

---
+++ sweet-shop.h
@@ -0,0 +1,8 @@
+#ifndef SWEET_SHOP_H_INCLUDED
+#define SWEET_SHOP_H_INCLUDED
+
+#define ERROR_BUFFER_SIZE               128
+
+#define DATABASE_PATH                   "data/goods.db"
+
+#endif // SWEET_SHOP_H_INCLUDED
