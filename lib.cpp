359aea7a575db752d869af4eccb4b8d023e9155b
jeka-js <jeka7js@gmail.com>
2013-03-03 14:57:21 +0200

    Different functions

---
+++ lib.cpp
@@ -0,0 +1,17 @@
+#include "lib.h"
+#include <cstring>
+#include <cctype>
+
+bool is_number( const char *str )
+{
+    bool res = true;
+
+    for( size_t i = 0; i < strlen( str ); i++ ) {
+        if( !isdigit( str[i] ) ) {
+            res = false;
+        }
+    }
+
+    return res;
+}
+
---
+++ lib.h
@@ -0,0 +1,6 @@
+#ifndef LIB_H_INCLUDED
+#define LIB_H_INCLUDED
+
+bool is_number( const char * );
+
+#endif // LIB_H_INCLUDED
